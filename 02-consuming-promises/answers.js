cd3e37ab1f772bba07a447757c0363a9ce090927
Oryanab <86202122+Oryanab@users.noreply.github.com>
2021-10-13 14:44:10 +0300

    update 2.0 - folder 2 solved

--- 02-consuming-promises/answers.js
+++ 02-consuming-promises/answers.js
@@ -1,24 +1,60 @@
+//  const changeColor = (color, time) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const randNum = Math.floor(Math.random() * 10 + 1);
+//       if (randNum > 0) {
+//         const body = document.body;
+//         body.style.backgroundColor = color;
+//         console.log(color);
+//         resolve();
+//       } else {
+//         reject();
+//       }
+//       return color;
+//     }, time);
+//   });
+// };
 /**
- * 
+ *
  * EXERCISE 1
- * 
+ *
  * @param {Promise} promise
  * @param {thunk} action
- * 
+ *
  */
-function waitForPromise(promise, action){
-  /* IMPLEMENT ME */
+function waitForPromise(promise, action) {
+  return new Promise((promise, action) => {
+    setTimeout(() => {
+      promise();
+    }, 1000);
+  }).then(() => {
+    action();
+  });
 }
 /**
- * 
+ *
  * EXERCISE 2
- * 
- * @param {Promise} promise 
- * @param {consumer} consumer 
- * @param {handler} handler 
+ *
+ * @param {Promise} promise
+ * @param {consumer} consumer
+ * @param {handler} handler
  */
-function consumePromise(promise, consumer, handler){
-  /* IMPLEMENT ME! */
+
+//  async function calc() {
+//throw "error"; // thorw you to reject
+// return "hey"; // get you into resolve (hey will go to data)
+// }
+function consumePromise(promise, consumer, handler) {
+  async function promis() {
+    return promise;
+  }
+  promis()
+    .then((result) => {
+      consumer(result);
+    })
+    .catch((err) => {
+      handler(err);
+    });
 }
 
 /**
